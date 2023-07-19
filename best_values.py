e7490874e3d52015142ccc8ef0a4655a9e7dfa9f
nzylakffa <131708217+nzylakffa@users.noreply.github.com>
2023-07-19 09:37:57 -0400

    Add files via upload
    Add in NFL and Underdog

--- best_values.py
+++ best_values.py
@@ -15,32 +15,44 @@ def scrape_espn_adp():
     response = requests.get(url)
     tables = pd.read_html(response.content)
     df = tables[0]
-    df = df[['Player', 'Team', 'Position', 'ESPN', 'Y!']]
+    df = df[['Player', 'Team', 'Position', 'ESPN', 'Y!', 'NFL', 'Underdog']]
     df[['Pos', 'Pos Rank']] = df['Position'].str.split('-', n=1, expand=True)
     # Keep just these columns
-    df = df[['Player', 'Team', 'Pos', 'ESPN', 'Y!']]
+    df = df[['Player', 'Team', 'Pos', 'ESPN', 'Y!', 'NFL', 'Underdog']]
     # Rename
     df = df.rename(columns={'Y!': 'Yahoo'})
     # Replace all "-" with 500
     df['ESPN'] = df['ESPN'].replace('-', 250, regex=True).astype(int)
     df['Yahoo'] = df['Yahoo'].replace('-', 250, regex=True).astype(int)
+    df['NFL'] = df['NFL'].replace('-', 250, regex=True).astype(int)
+    df['Underdog'] = df['Underdog'].replace('-', 250, regex=True).astype(int)
     # If an ADP is > 250, make it 250
     df['ESPN'] = np.where(df['ESPN']>250,250,df['ESPN'])
     df['Yahoo'] = np.where(df['Yahoo']>250,250,df['Yahoo'])
+    df['NFL'] = np.where(df['NFL']>250,250,df['NFL'])
+    df['Underdog'] = np.where(df['Underdog']>250,250,df['Underdog'])   
     # Make columns numeric
     df['ESPN'] = pd.to_numeric(df['ESPN'])
     df['Yahoo'] = pd.to_numeric(df['Yahoo'])
+    df['NFL'] = pd.to_numeric(df['NFL'])
+    df['Underdog'] = pd.to_numeric(df['Underdog'])
     # Fill na team with FA
     df['Team'].fillna("FA", inplace=True)
     df['Team'] = df['Team'].replace('-', "FA", regex=True)
     # Create Average ADP Column
-    df['Average ADP'] = (df['ESPN'] + df['Yahoo'])/2
+    df['Average ADP'] = (df['ESPN'] + df['Yahoo'] + df['NFL'] + df['Underdog'])/4
     # Sort by Average ADP
     df.sort_values(by='Average ADP', inplace=True)
     # Best ADP
-    df['Best ADP Site'] = np.where(df['ESPN'] > df['Yahoo'], "ESPN", "Yahoo")
+    df['Best ADP Site'] = np.where((df['ESPN'] >= df['Yahoo']) & (df['ESPN'] >= df['NFL']) & (df['ESPN'] >= df['Underdog']), "ESPN",
+                                  (np.where((df['Yahoo'] >= df['ESPN']) & (df['Yahoo'] >= df['NFL']) & (df['Yahoo'] >= df['Underdog']),'Yahoo',
+                                           (np.where((df['NFL'] >= df['ESPN']) & (df['NFL'] >= df['Yahoo']) & (df['NFL'] >= df['Underdog']),'NFL',
+                                                    'Underdog')))))
     # Worst ADP
-    df['Worst ADP Site'] = np.where(df['ESPN'] < df['Yahoo'], "ESPN", "Yahoo")
+    df['Worst ADP Site'] = np.where((df['ESPN'] <= df['Yahoo']) & (df['ESPN'] <= df['NFL']) & (df['ESPN'] <= df['Underdog']), "ESPN",
+                                  (np.where((df['Yahoo'] <= df['ESPN']) & (df['Yahoo'] <= df['NFL']) & (df['Yahoo'] <= df['Underdog']),'Yahoo',
+                                           (np.where((df['NFL'] <= df['ESPN']) & (df['NFL'] <= df['Yahoo']) & (df['NFL'] <= df['Underdog']),'NFL',
+                                                    'Underdog')))))
     # Reset Index
     df = df.reset_index(drop=True)
     return df
@@ -65,10 +77,10 @@ if st.checkbox("Get ADP's"):
                                   ["QB", "RB", "WR", "TE", "DST"], ["QB", "RB", "WR", "TE"])
         adp_df = adp_df[adp_df['Pos'].isin(filtered)]
         best_values = st.multiselect("Best Values by Site",
-                                    ["ESPN", "Yahoo"], ["ESPN", "Yahoo"])
+                                    ["ESPN", "Yahoo", "NFL", "Underdog"], ["ESPN", "Yahoo", "NFL", "Underdog"])
         adp_df = adp_df[adp_df['Best ADP Site'].isin(best_values)]
         worst_values = st.multiselect("Worst Values by Site",
-                                    ["ESPN", "Yahoo"], ["ESPN", "Yahoo"])
+                                    ["ESPN", "Yahoo", "NFL", "Underdog"], ["ESPN", "Yahoo", "NFL", "Underdog"])
         adp_df = adp_df[adp_df['Worst ADP Site'].isin(worst_values)]
         adp_df['Average ADP'] = round(adp_df['Average ADP'],2)
         st.dataframe(adp_df.style.text_gradient(cmap=cm).format({'Average ADP':'{:.1f}'}).apply(highlight_rows, axis=1))
@@ -83,12 +95,9 @@ if st.checkbox("Get ADP's"):
         
 
 ### Sidebar ###
-st.sidebar.image('ffa_red.png', use_column_width=True)
+# st.sidebar.image('ffa_red.png', use_column_width=True)
 st.sidebar.markdown(" ## About This App:")
 st.sidebar.markdown("This app will scrape the current indusrty ADP's and display them in ways where you can evaluate who the best picks are on each site!")
-
-st.sidebar.markdown("## Note:")
-st.sidebar.markdown("My goal is to have Sleeper, CBS and NFL all on this table as well. Just going to take some time to get the data for those running properly. I'm also going to record a video soon going over how to best use this app. I'll post that as a link here when I have that done. I'll probably wait until I finish getting the ADP data from the other sites though.")
 
 st.sidebar.markdown("## Steps:")
 st.sidebar.markdown("1) Click the checkbox next to: Get ADP's.")
