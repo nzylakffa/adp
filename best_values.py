9793cce08b76571d36eb45cf547c2ada1f9f39ec
nzylakffa <131708217+nzylakffa@users.noreply.github.com>
2025-08-01 12:52:35 +0000

    fix NFL

--- best_values.py
+++ best_values.py
@@ -38,26 +38,23 @@ def scrape_espn_adp():
     response = requests.get(url)
     tables = pd.read_html(response.content)
     df = tables[0]
-    df = df[['Player', 'Team', 'Position', 'ESPN', 'NFL', 'Underdog']]
+    df = df[['Player', 'Team', 'Position', 'ESPN', 'Underdog']]
     df[['Pos', 'Pos Rank']] = df['Position'].str.split('-', n=1, expand=True)
     # Keep just these columns
-    df = df[['Player', 'Team', 'Pos', 'ESPN', 'NFL', 'Underdog']]
+    df = df[['Player', 'Team', 'Pos', 'ESPN', 'Underdog']]
     # Rename
     # df = df.rename(columns={'Y!': 'Yahoo'})
     # Replace all "-" with 500
     df['ESPN'] = df['ESPN'].replace('-', 250, regex=True).astype(int)
     # df['Yahoo'] = df['Yahoo'].replace('-', 250, regex=True).astype(int)
-    df['NFL'] = df['NFL'].replace('-', 250, regex=True).astype(int)
     df['Underdog'] = df['Underdog'].replace('-', 250, regex=True).astype(int)
     # If an ADP is > 250, make it 250
     df['ESPN'] = np.where(df['ESPN']>250,250,df['ESPN'])
     # df['Yahoo'] = np.where(df['Yahoo']>250,250,df['Yahoo'])
-    df['NFL'] = np.where(df['NFL']>250,250,df['NFL'])
     df['Underdog'] = np.where(df['Underdog']>250,250,df['Underdog'])   
     # Make columns numeric
     df['ESPN'] = pd.to_numeric(df['ESPN'])
     # df['Yahoo'] = pd.to_numeric(df['Yahoo'])
-    df['NFL'] = pd.to_numeric(df['NFL'])
     df['Underdog'] = pd.to_numeric(df['Underdog'])
     # Fill na team with FA
     df['Team'].fillna("FA", inplace=True)
@@ -68,22 +65,20 @@ def scrape_espn_adp():
     df['Yahoo'] = pd.to_numeric(df['Yahoo'])
 
     # Create Average ADP Column
-    df['Average ADP'] = (df['ESPN'] + df['Yahoo'] + df['NFL'] + df['Underdog'] + df['Sleeper'])/5
+    df['Average ADP'] = (df['ESPN'] + df['Yahoo'] + df['Underdog'] + df['Sleeper'])/4
     # Sort by Average ADP
     df.sort_values(by='Average ADP', inplace=True)
     # Best ADP
-    df['Best ADP Site'] = np.where((df['ESPN'] >= df['Yahoo']) & (df['ESPN'] >= df['NFL']) & (df['ESPN'] >= df['Underdog']) & (df['ESPN'] >= df['Sleeper']), "ESPN",
-                                  (np.where((df['Yahoo'] >= df['ESPN']) & (df['Yahoo'] >= df['NFL']) & (df['Yahoo'] >= df['Underdog']) & (df['Yahoo'] >= df['Sleeper']),'Yahoo',
-                                           (np.where((df['NFL'] >= df['ESPN']) & (df['NFL'] >= df['Yahoo']) & (df['NFL'] >= df['Underdog']) & (df['NFL'] >= df['Sleeper']),'NFL',
-                                                     (np.where((df['Sleeper'] >= df['ESPN']) & (df['Sleeper'] >= df['Yahoo']) & (df['Sleeper'] >= df['NFL']) & (df['Sleeper'] >= df['Underdog']),'Sleeper', 'Underdog')))))))
+    df['Best ADP Site'] = np.where((df['ESPN'] >= df['Yahoo']) & (df['ESPN'] >= df['Underdog']) & (df['ESPN'] >= df['Sleeper']), "ESPN",
+                                  (np.where((df['Yahoo'] >= df['ESPN']) & (df['Yahoo'] >= df['Underdog']) & (df['Yahoo'] >= df['Sleeper']),'Yahoo',
+                                                     (np.where((df['Sleeper'] >= df['ESPN']) & (df['Sleeper'] >= df['Yahoo']) & (df['Sleeper'] >= df['Underdog']),'Sleeper', 'Underdog')))))
     
     
     
     # Worst ADP
-    df['Worst ADP Site'] = np.where((df['ESPN'] <= df['Yahoo']) & (df['ESPN'] <= df['NFL']) & (df['ESPN'] <= df['Underdog']) & (df['ESPN'] <= df['Sleeper']), "ESPN",
-                                  (np.where((df['Yahoo'] <= df['ESPN']) & (df['Yahoo'] <= df['NFL']) & (df['Yahoo'] <= df['Underdog']) & (df['Yahoo'] <= df['Sleeper']),'Yahoo',
-                                           (np.where((df['NFL'] <= df['ESPN']) & (df['NFL'] <= df['Yahoo']) & (df['NFL'] <= df['Underdog']) & (df['NFL'] <= df['Sleeper']),'NFL',
-                                                     (np.where((df['Sleeper'] <= df['ESPN']) & (df['Sleeper'] <= df['Yahoo']) & (df['Sleeper'] <= df['NFL']) & (df['Sleeper'] <= df['Underdog']),'Sleeper', 'Underdog')))))))
+    df['Worst ADP Site'] = np.where((df['ESPN'] <= df['Yahoo']) & (df['ESPN'] <= df['Underdog']) & (df['ESPN'] <= df['Sleeper']), "ESPN",
+                                  (np.where((df['Yahoo'] <= df['ESPN']) & (df['Yahoo'] <= df['Underdog']) & (df['Yahoo'] <= df['Sleeper']),'Yahoo',
+                                                     (np.where((df['Sleeper'] <= df['ESPN']) & (df['Sleeper'] <= df['Yahoo']) & (df['Sleeper'] <= df['Underdog']),'Sleeper', 'Underdog')))))
     
 
     # Reset Index
@@ -98,7 +93,7 @@ def scrape_espn_adp():
     # Combine position and pos rank
     df['Final Pos Rank'] = df['Pos'].astype(str) + " " + df['Pos Rank'].astype(str)
     # Order the columns how we want them
-    df = df[["Average ADP", "Player", "Team", "Pos", "Final Pos Rank", "ESPN", "Yahoo", "NFL", "Underdog", "Sleeper", "Best ADP Site", "Worst ADP Site"]]
+    df = df[["Average ADP", "Player", "Team", "Pos", "Final Pos Rank", "ESPN", "Yahoo", "Underdog", "Sleeper", "Best ADP Site", "Worst ADP Site"]]
     # Rename Final Pos Rank
     df = df.rename(columns={'Final Pos Rank': 'Pos Rank'})    
     return df
@@ -123,10 +118,10 @@ if st.checkbox("Get ADP's"):
                                   ["QB", "RB", "WR", "TE", "DST"], ["QB", "RB", "WR", "TE"])
         adp_df = adp_df[adp_df['Pos'].isin(filtered)]
         best_values = st.multiselect("Best Values by Site",
-                                    ["ESPN", "Yahoo", "NFL", "Underdog", "Sleeper"], ["ESPN", "Yahoo", "NFL", "Underdog", "Sleeper"])
+                                    ["ESPN", "Yahoo", "Underdog", "Sleeper"], ["ESPN", "Yahoo", "Underdog", "Sleeper"])
         adp_df = adp_df[adp_df['Best ADP Site'].isin(best_values)]
         worst_values = st.multiselect("Worst Values by Site",
-                                    ["ESPN", "Yahoo", "NFL", "Underdog", "Sleeper"], ["ESPN", "Yahoo", "NFL", "Underdog", "Sleeper"])
+                                    ["ESPN", "Yahoo", "Underdog", "Sleeper"], ["ESPN", "Yahoo", "Underdog", "Sleeper"])
         adp_df = adp_df[adp_df['Worst ADP Site'].isin(worst_values)]
         adp_df['Average ADP'] = round(adp_df['Average ADP'],2)
         adp_df['Sleeper'] = round(adp_df['Sleeper'],2)
